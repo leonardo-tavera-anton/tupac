9d4268480ce396ca0ef3f9946ed064e53ac4bb83
Leonardo Tavera <leonardo.tavera.anton@outlook.com>
2026-02-17 09:15:48 -0500

    si

--- backend/app/Http/Controllers/RequisitoController.php
+++ backend/app/Http/Controllers/RequisitoController.php
@@ -7,41 +7,26 @@ use Illuminate\Http\Request;
 
 class RequisitoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Requisito $requisito)
     {
         //
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Requisito $requisito)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Requisito $requisito)
     {
         //
