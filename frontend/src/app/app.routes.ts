ae766a7297aa48f1b489f78feab3fe4dd791e4a1
Leonardo Tavera <leonardo.tavera.anton@outlook.com>
2026-02-19 10:35:25 -0500

    aaaa

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
 import { TupaComponent } from './pages/tupa/tupa'; // <--- Cambiado de Tupa a TupaComponent
 
 export const routes: Routes = [
@@ -12,16 +11,4 @@ export const routes: Routes = [
     redirectTo: 'tupa',
     pathMatch: 'full'
   }
-=======
-import { Login } from './pages/login/login';
-import { Tupa } from './pages/tupa/tupa';
-import { Register } from './pages/register/register'; // 1. Importa el componente
-
-export const routes: Routes = [
-  { path: 'login', component: Login },
-  { path: 'register', component: Register }, // 2. Añade la ruta de registro
-  { path: 'tupa', component: Tupa },
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: '**', redirectTo: 'login' } 
->>>>>>> aa2fccf0d093e4b2c927b6f4cc309afe767a5bb5
 ];
